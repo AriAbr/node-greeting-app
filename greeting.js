3cab5e2d5f94bbcaec5a980471ac3fc56914339b
Ari <ari.abramowitz1@gmail.com>
2019-03-19 13:06:19 -0400

    checkpoint 2976 basic node app: checkpoint work

---
+++ greeting.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   greetingEnglish: function() {
+//     console.log("Hello");
+//   },
+//   greetingJapanese: function() {
+//     console.log("Konichiwa");
+//   },
+// };
+
+exports.greetingEnglish = function() {
+  console.log("Hello");
+}
+exports.greetingJapanese = function() {
+  console.log("Konichiwa");
+}
+
+exports = 'hola';
