ef888d8abdc3e7571848d708be12d7026823bb66
Ari <ari.abramowitz1@gmail.com>
2019-03-19 13:21:56 -0400

    checkpoint 2967 basic node app: completed

--- greeting.js
+++ greeting.js
@@ -1,17 +1,15 @@
-// module.exports = {
-//   greetingEnglish: function() {
-//     console.log("Hello");
-//   },
-//   greetingJapanese: function() {
-//     console.log("Konichiwa");
-//   },
-// };
-
 exports.greetingEnglish = function() {
-  console.log("Hello");
-}
-exports.greetingJapanese = function() {
-  console.log("Konichiwa");
+ console.log("Hello");
 }
 
-exports = 'hola';
+exports.greetingJapanese = function() {
+ console.log("Konichiwa");
+}
+
+exports.greetingSpanish = function() {
+  console.log("Hola");
+};
+
+exports.greetingIcelandic = function() {
+  console.log("Halló");
+}
--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const greetings = require('./greeting.js');
+const greetings = require("./greeting.js");
+const farewells = require("./farewells.js");
 
 // "Hello"
 greetings.greetingEnglish();
 
 // "Konichiwa"
 greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+farewells.farewellEnglish();
+
+farewells.farewellSpanish();
